3c3bce5c3831cec3958dbbcba29026e8fe941a87
Mikhailo2004 <prodazha2k18@gmail.com>
2025-06-22 14:08:25 +0000

    Add stake version

---
+++ stake/track.php
@@ -0,0 +1,16 @@
+<?php
+$event = $_GET['event'] ?? 'visit';
+$ip = $_SERVER['REMOTE_ADDR'] ?? '';
+$userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+$country = 'Unknown';
+try {
+    $info = @file_get_contents("https://ipapi.co/{$ip}/country_name/");
+    if ($info) {
+        $country = trim($info);
+    }
+} catch (Exception $e) {
+}
+$line = date('c')."\t{$event}\t{$ip}\t{$country}\t{$userAgent}\n";
+file_put_contents('visitors.log', $line, FILE_APPEND);
+echo 'ok';
+?>
